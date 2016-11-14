0a64dc0b675d64bb5c3dafea5afe947c7539ab0d
rowland17 <rowland17@up.edu>
2016-11-13 22:43:54 -0800

    issues with socket connection.

--- Main.java
+++ Main.java
@@ -2,6 +2,8 @@ import java.applet.*;
 import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
+import java.net.*;
+import java.io.*;
 /**
  * Write a description of class Main here.
  * 
@@ -25,6 +27,7 @@ public class Main extends Applet implements ActionListener
     int buttonHeight = 40;
     boolean b0Displayed = false;
     boolean isServer = true;
+    String s = "";
     public void init() {
         startButton = new Button("Start Game");
         add(startButton);
@@ -109,11 +112,113 @@ public class Main extends Applet implements ActionListener
 
         if(n == JOptionPane.YES_OPTION){
             isServer = true;
-            JOptionPane.showMessageDialog(null,new String("Aledafffffrt!!"));
+            serverCode();
         }
-        else
+        else{
             isServer = false;
+            s = (String)JOptionPane.showInputDialog(
+                    null,
+                    "What is the ip address of the server player?",
+                    "Customized Dialog",
+                    JOptionPane.PLAIN_MESSAGE,
+                    null,
+                    null,
+                    "");
+
+            //If a string was returned, say so.
+            if ((s != null) && (s.length() > 0)) {
+                
+            }
+
+            clientCode();
+        }
+
+    }
+
+    /*
+     * used Tammy's lab 1 tcp server code
+     */
+    public void serverCode()
+    {
+        String clientMessage, serverReply;
+
+        try {
+            // create socket connection to port 9999
+            ServerSocket accepting = new ServerSocket(9999);
+
+            // wait for clients to make connections
+            while(true) {
+                Socket connectionSocket = accepting.accept();
+                BufferedReader clientIn = new BufferedReader(
+                        new InputStreamReader(connectionSocket.getInputStream()));
+
+                DataOutputStream clientOut = 
+                    new DataOutputStream(connectionSocket.getOutputStream());
+
+                // get message from client and captilatize the letters
+                clientMessage = clientIn.readLine();
+                JOptionPane.showMessageDialog(null,"Got the message: " + clientMessage);
+                serverReply = "Ack for client at " + connectionSocket.getInetAddress() + ": " + clientMessage.toUpperCase() + "\n";
+
+                // send client reply 
+                clientOut.writeBytes(serverReply);
+                connectionSocket.close();
+            }
+        }
+        catch (Exception e) {
             
+            JOptionPane.showMessageDialog(null,e.toString() + "\nAn error occurred while creating server socket or reading/writing data to/from client.");
+            System.out.println("An error occurred while creating server socket or reading/writing data to/from client.");
+        }
+    }
+
+    /*
+     * tammy's client code lab 1
+     */
+    public void clientCode()
+    {
+        // create Strings to store message and reply
+        String message, reply;
+
+        try {
+            // create reader to acquire text
+            BufferedReader userIn = new BufferedReader(
+                    new InputStreamReader(System.in));
+
+            // get message from user
+            message = userIn.readLine();
+
+            // create socket connection on current machine at port 9999
+            // and attach stream for writing data
+            // connection is made to localhost (same machine), but could 
+            // instead put in 32-bit or 128-bit unsigned number
+            // for IP address or a string with the host name
+            Socket clientSocket = new Socket(s, 9999);
+            DataOutputStream serverOut = 
+                new DataOutputStream(clientSocket.getOutputStream());
+
+            // create stream for reading data from server
+            BufferedReader serverIn = new BufferedReader(
+                    new InputStreamReader(clientSocket.getInputStream()));
+
+            // send data to server
+            serverOut.writeBytes(message + '\n');
+            // get data from server
+            reply = serverIn.readLine();
+            // print reply from server
+            
+            JOptionPane.showMessageDialog(null,"REPLY RECEIVED: " + reply);
+            System.out.println("REPLY RECEIVED: " + reply);
+            clientSocket.close();
+        }
+        catch(ConnectException e) {
+            JOptionPane.showMessageDialog(null,"Error connecting to server. Check that server is running and accepting connections.");
+            System.out.println("Error connecting to server. Check that server is running and accepting connections.");
+        }
+        catch(Exception e) {
+             JOptionPane.showMessageDialog(null, "Exception occurred: " + e.getMessage());
+            System.out.println("Exception occurred: " + e.getMessage());
+        }
     }
     Button startButton, endButton, connectButton, turnButton;
     Button b0, b1, b2, b3, b4, b5, b6, b7, b8;
